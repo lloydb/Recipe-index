dff006f1b701e8436308c36e7f52afb5475e0e63
lloyd burgess <heroku@lloyd.clara.net>
2010-09-08 15:37:55 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lrecipe::Application.config.secret_token = 'f6704ef5d11d6f2691696b6d63033c160efe1fa94b69f072e8acf42cfb1acf037bac27ea1877819fddf9c05dbcbb4bdd7c40a0d2672ae8b4e99a02ca64201fea'
